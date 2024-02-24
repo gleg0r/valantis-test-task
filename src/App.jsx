f15b9ce6305102fbc04c300d6d4d11b3a49bd764
gleg0r <egor103230@gmail.com>
2024-02-24 21:22:25 +0300

    try to upload on netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { fetchData } from './store/slices/apiSlice';
 import ProductList from './components/ProductList/ProductList';
 
 function App() {
   const offset = useSelector(state => state.getData.currentPage);
   const limit = useSelector(state => state.getData.productsPerPage);
-  const itemsId = useSelector(state => state.getData.ids)
   const status = useSelector(state => state.getData.status)
   const statusGetIds = useSelector(state => state.getData.statusGetIds);
   const dispatch = useDispatch();
@@ -16,7 +15,7 @@ function App() {
     if(statusGetIds !== 'resolved' && status !== 'loading') {
       dispatch(fetchData({action: "get_ids", params: {"offset": offset, "limit": limit}}))
     }
-  }, [dispatch, statusGetIds, offset, limit]);
+  }, [dispatch, statusGetIds, status, offset, limit]);
 
   // function prevPage() {
   //   if(offset === 0) return;
